7e0bc9d58cc477fac5e5328dc4946e4b871bcfd7
Coops023 <bjorkc27@gmail.com>
2022-03-08 13:55:50 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Headphones from "./pages/Headphones";
@@ -7,17 +7,33 @@ import Earphones from "./pages/Earphones";
 import Navbar from "./component/Navbar";
 import Footer from "./component/Footer";
 import ProductDetails from "./pages/ProductDetails";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import ScrollToTop from "./component/ScrollToTop";
 
 function App() {
+  const [cart, setCart] = useState(false);
+
+  const showCart = () => {
+    if (cart) {
+      setCart(false);
+    } else {
+      setCart(true);
+    }
+  };
+
   return (
     <div className="App">
-      <Navbar />
+      <ScrollToTop />
+      <Navbar showCart={showCart} />
+      {!cart ? "" : <Cart showCart={showCart} />}
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/headphones" element={<Headphones />} />
         <Route exact path="/earphones" element={<Earphones />} />
         <Route exact path="/speakers" element={<Speakers />} />
         <Route exact path="/product/:id" element={<ProductDetails />} />
+        <Route exact path="/checkout" element={<Checkout />} />
       </Routes>
       <Footer />
     </div>
