3766cb845d1238c832f280498d6be16810320272
Coops023 <bjorkc27@gmail.com>
2022-03-08 19:21:46 +0100

    begin stripe integration

--- routes/route.js
+++ routes/route.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 const Product = mongoose.model("Product");
 const { Types } = require("mongoose");
 
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+
 module.exports = (app) => {
   app.get("/all-products", async (req, res) => {
     try {
@@ -57,5 +59,55 @@ module.exports = (app) => {
     } catch (err) {
       res.status(500).json(err);
     }
+  });
+
+  app.get("/stripe", (req, res) => {
+    res.send(stripe);
+  });
+
+  app.post("/checkout", async (req, res) => {
+    console.log("Request:", req.body);
+
+    let error;
+    let status;
+    try {
+      const { product, token } = req.body;
+
+      const customer = await stripe.customers.create({
+        email: token.email,
+        source: token.id,
+      });
+
+      const idempotency_key = uuid();
+      const charge = await stripe.charges.create(
+        {
+          amount: product.price * 100,
+          currency: "usd",
+          customer: customer.id,
+          receipt_email: token.email,
+          description: `Purchased the ${product.name}`,
+          shipping: {
+            name: token.card.name,
+            address: {
+              line1: token.card.address_line1,
+              line2: token.card.address_line2,
+              city: token.card.address_city,
+              country: token.card.address_country,
+              postal_code: token.card.address_zip,
+            },
+          },
+        },
+        {
+          idempotency_key,
+        }
+      );
+      console.log("Charge:", { charge });
+      status = "success";
+    } catch (error) {
+      console.error("Error:", error);
+      status = "failure";
+    }
+
+    res.json({ error, status });
   });
 };
